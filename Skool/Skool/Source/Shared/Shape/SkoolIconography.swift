b1b39723b23d4c63f88dd1f7876f7c2b71090a6b
eunchan2815 <164847037+eunchan2815@users.noreply.github.com>
2024-11-22 22:06:54 +0900

    🚧 (:construction:) - 초기코드 업로드

---
+++ Skool/Skool/Source/Shared/Shape/SkoolIconography.swift
@@ -0,0 +1,27 @@
+//
+//  SkoolIconography.swift
+//  Skool
+//
+//  Created by dgsw30 on 11/22/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum SkoolIconography: String {
+    case TimeTable = "TimeTable"
+    case Seat = "Seat"
+    case meal = "Meal"
+}
+
+extension Image {
+    init(icon: SkoolIconography) {
+        self = Image(icon.rawValue)
+    }
+}
+
+struct SkoolTests {
+    var body: some View {
+        Image(icon: .Seat)
+    }
+}
