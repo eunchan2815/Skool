b16d13756f89e04b7e47b8d7fb579b9445d5f2d0
eunchan2815 <164847037+eunchan2815@users.noreply.github.com>
2024-11-22 21:53:50 +0900

    🎉 (:tada:) - 프로젝트 시작

---
+++ Skool/Skool/SkoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkoolApp.swift
+//  Skool
+//
+//  Created by dgsw30 on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SkoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
